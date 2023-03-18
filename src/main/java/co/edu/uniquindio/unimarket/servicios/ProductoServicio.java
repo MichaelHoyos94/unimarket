b2c687de2a19415b31ca23b2913be2c5f587f6c4
MichaelHoyos94 <maaguirreh@uqvirtual.edu.co>
2023-03-17 20:34:11 -0500

    Servicios producto

---
+++ src/main/java/co/edu/uniquindio/unimarket/servicios/ProductoServicio.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.unimarket.servicios;
+
+public interface ProductoServicio {
+    crearProducto();
+    eliminarProducto();
+    actualizarProducto();
+    actualizarProductoEstado();
+    actualizarProductoCantidades();
+    listarProductosCategoria();
+    listarProductosUsuario();
+    listarProductosEstado();
+    listarProductosNombre();
+    listarProductosPrecio();
+    listarProductoCodigo();
+}
